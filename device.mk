9d7d5b5f07f83e44252f20724bcda74eca77eb37
Daniel Rosenberg <drosen@google.com>
2021-11-04 08:06:26 +0000

    whyred: Enable casefolding support on userdata
    This enables casefolding, which is required for removal of sdcardfs.
    
    Test: Wipe data and boot. lsattr /data/media, check for +F
    Bug: 138322712
    Change-Id: I020aa6bb9c121e7bda83c07bc9fb37dcfa26702c

--- device.mk
+++ device.mk
@@ -2,6 +2,9 @@
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # qcom decryption
 PRODUCT_PACKAGES_ENG += \
     qcom_decrypt \
