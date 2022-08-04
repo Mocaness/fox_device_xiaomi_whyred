25b9971e11f26db62a10a18ae16043a570f71314
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-05 05:32:39 +0800

    whyred: Build some debugging utils

--- device.mk
+++ device.mk
@@ -1,3 +1,12 @@
+# Debug
+PRODUCT_PACKAGES += \
+    crash_dump \
+    libprocinfo.recovery
+
+PRODUCT_COPY_FILES += \
+    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/system/apex/com.android.runtime/bin/crash_dump32:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/crash_dump32 \
+    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/system/apex/com.android.runtime/bin/crash_dump64:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/crash_dump64
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
