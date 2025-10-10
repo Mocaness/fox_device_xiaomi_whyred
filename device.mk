a97e15e0166922b539a4b02747cb31926af4bc15
Yukizaki Miyamura <yukizakimiyamura@gmail.com>
2025-10-10 19:27:08 +0700

    whyred: enable fastbootd
    Signed-off-by: Yukizaki Miyamura <yukizakimiyamura@gmail.com>

--- device.mk
+++ device.mk
@@ -5,6 +5,21 @@ PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Fastbootd
+TW_INCLUDE_FASTBOOTD := true
+PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
+
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-service \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.fastbootd.available=true
+
 # qcom decryption
 PRODUCT_PACKAGES_ENG += \
     qcom_decrypt \
