88b074abca639db4e35a398fc5a43fd1a8eab2e7
Mocarafee <harif.rp05@gmail.com>
2025-10-16 16:22:07 +0700

    Revert "build gatekeeper and keymaster from source"
    This reverts commit 6681637d279620b0b40c060a0b94eb7abfd07358.

--- device.mk
+++ device.mk
@@ -24,15 +24,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES_ENG += \
     qcom_decrypt \
     qcom_decrypt_fbe
-
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
-    android.hardware.keymaster@3.0.vendor
-
-# Gatekeeper HAL
-PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service \
-    android.hardware.gatekeeper@1.0.vendor
