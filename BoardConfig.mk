7abb54e67c96c3dd1c7ee12764e6a61a462b6c2d
Mocarafee <harif.rp05@gmail.com>
2025-10-21 10:50:38 +0700

    BoardConfig: Change the path of cpu temperature
    * Since 4.19, pm660-tz doesn't show the correct temperature (always 99-100)
    * Now it's using pm660l-tz

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,6 +114,7 @@ RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_DEFAULT_BRIGHTNESS := 1433
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone1/temp"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_CRYPTO := true
