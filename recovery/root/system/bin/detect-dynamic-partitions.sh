d0dae863cf8b1bbaaef78f0b0f9adce9aa8c3967
Mocarafee <harif.rp05@gmail.com>
2025-10-26 10:35:28 +0700

    whyred: enable casefold on 4.19

--- recovery/root/system/bin/detect-dynamic-partitions.sh
+++ recovery/root/system/bin/detect-dynamic-partitions.sh
@@ -3,6 +3,11 @@
 if dd if=/dev/block/by-name/system bs=256k count=1|strings|grep qti_dynamic_partitions > /dev/null; then
     # Dynamic partitions
     cat /system/etc/recovery.fstab.dynamic >> /system/etc/recovery.fstab
+   
+    # Enable Casefold support on 4.19
+    resetprop external_storage.casefold.enabled 1
+    resetprop external_storage.projid.enabled 1
+    resetprop external_storage.sdcardfs.enabled 0 
 else
     # Normal partition
     cat /system/etc/twrp.flags.normal >> /system/etc/twrp.flags
